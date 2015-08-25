08b8b1050b2884f16466fe95bfc0c9a36d415896
Butch Landingin <butchland@gmail.com>
2015-08-25 19:24:22 +0800

    minor reformatting

--- tests/testthat/test_dataframe_stats_generation.R
+++ tests/testthat/test_dataframe_stats_generation.R
@@ -3,8 +3,8 @@ library(Rbutchland)
 context("dataframe stats")
 
 test_that("summarize dataframe with df_stat", {
-  df = data.frame(x=c(1,2,3),y=c("a","b","c"))
-  df_box = df_boxstat(df)
+  df <- data.frame(x=c(1,2,3),y=c("a","b","c"))
+  df_box <- df_boxstat(df)
   expect_equal(df_box$dimension, c("nrows","ncols"))
   expect_equal(df_box$value, c(3,2))
 })
