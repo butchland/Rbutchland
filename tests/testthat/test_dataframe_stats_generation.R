f9fe32630fc30798a2c2ac12079b559aa8ec8b3a
Butch Landingin <butchland@gmail.com>
2015-08-25 22:30:57 +0800

    add tests for column stats

--- tests/testthat/test_dataframe_stats_generation.R
+++ tests/testthat/test_dataframe_stats_generation.R
@@ -2,9 +2,18 @@ library(Rbutchland)
 
 context("dataframe stats")
 
-test_that("summarize dataframe with df_stat", {
+test_that("summarize dataframe with df_boxstat", {
   df <- data.frame(x=c(1,2,3),y=c("a","b","c"))
   df_box <- df_boxstat(df)
   expect_equal(df_box$dimension, c("nrows","ncols"))
   expect_equal(df_box$value, c(3,2))
+})
+
+test_that("summarize dataframe with df_columnstat", {
+  df <- data.frame(x=c(1,2,3),y=c("a","b",NA),z=c("a","b",NA))
+  df$z = as.character(df$z)
+  df_col <- df_columnstat(df)
+  expect_equal(df_col$column, c("x","y","z"))
+  expect_equal(df_col$type, c("numeric","factor","character"))
+  expect_equal(df_col$na, c(0,1,1))
 })
