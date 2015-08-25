2010d95cb09ba2aad00f69d36fe17bbae757256a
Butch Landingin <butchland@gmail.com>
2015-08-25 19:20:56 +0800

    added package level documentation

---
+++ R/Rbutchland.R
@@ -0,0 +1,7 @@
+#' @title My Package of Useful and (hopefully) Reusable Routines
+#' @name Rbutchland
+#' @description A bunch of useful and (hopefully) reusable routines
+#' to make every data scientist's life a little bit easier
+#' @docType package
+#' @aliases Rbutchland
+NULL
