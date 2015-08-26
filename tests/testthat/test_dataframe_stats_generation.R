4c75e02bfdd641cf6b19e41f0de0b44168a2b369
Butch Landingin <butchland@gmail.com>
2015-08-26 15:38:56 +0800

    added test to check for warnings

--- tests/testthat/test_dataframe_stats_generation.R
+++ tests/testthat/test_dataframe_stats_generation.R
@@ -11,9 +11,16 @@ test_that("summarize dataframe with df_boxstat", {
 
 test_that("summarize dataframe with df_columnstat", {
   df <- data.frame(x=c(1,2,3),y=c("a","b",NA),z=c("a","b",NA))
-  df$z = as.character(df$z)
+  df$z <- as.character(df$z)
   df_col <- df_columnstat(df)
   expect_equal(df_col$column, c("x","y","z"))
   expect_equal(df_col$type, c("numeric","factor","character"))
   expect_equal(df_col$na, c(0,1,1))
+})
+
+test_that("df_columnstat should work with diamonds with no warnings", {
+  library(ggplot2)
+  data(diamonds)
+  errhandler <- function(c) fail("should'nt throw warning")
+  tryCatch(df_columnstat(diamonds), warning=errhandler)
 })
